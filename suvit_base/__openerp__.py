863fcb8d700690f59c0395b5aa5286371b63209b
Victor Safronovich <vsafronovich@gmail.com>
2014-12-25 14:23:52 +0500

    initial module

---
+++ suvit_base/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'СУВИТ. Ядро',
+    'version': '0.0.1',
+    'category': 'SUVIT',
+    'complexity': 'easy',
+    'description': """
+
+Базовые вещи нужные во всех проектах:
+* Убирание плашки регистрации
+TODO
+    """,
+    'author': 'Suvit LLC',
+    'depends': ['base'],
+    'data': [
+    ],
+    'js': [
+    ],
+    'installable': True,
+    'auto_install': True,
+}
