4d2e8ef40eaf7a1a260b47d0d158309b78837f5e
Victor Safronovich <vsafronovich@gmail.com>
2015-02-17 19:50:02 +0500

    translate for module website

---
+++ suvit_website/__openerp__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Сувит. Сайт',
+    'category': 'Website',
+    'summary': 'Перевод модуля сайт',
+    'website': 'https://www.odoo.com/page/website-builder',
+    'version': '1.0',
+    'description': """
+Перевод Odoo модуля "Сайт"
+===========================
+
+        """,
+    'author': 'Suvit LLC',
+    'depends': ['website'],
+    'installable': True,
+    'auto_install': True,
+}
