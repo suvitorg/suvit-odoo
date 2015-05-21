cb6090d540e4679dfd84c811fbefc6da4500afaf
Victor Safronovich <vsafronovich@gmail.com>
2015-05-21 19:08:07 +0500

    working version of suvit_web_list_hide_cols

--- suvit_web_list_hide_cols/static/src/js/suvit_web_widgets.js
+++ suvit_web_list_hide_cols/static/src/js/suvit_web_widgets.js
@@ -1,2 +1,22 @@
 openerp.suvit_web_widgets = function(instance, local) {
+
+instance.web.ListView.include({
+    load_list: function(data) {
+        var self = this;
+        this._super(data);
+
+        this.$el.find('.oe_view_list_menu li input').click(function(){
+            checkbox = $(this);
+            _.map(self.fields_view.arch.children, function(field){
+                if (field.attrs.name == checkbox.data('field')) {
+                    var modifiers = JSON.parse(field.attrs.modifiers);
+                    modifiers['tree_invisible'] = !checkbox.prop('checked')? '1': null;
+                    field.attrs.modifiers = JSON.stringify(modifiers);
+                }
+            });
+            self.load_list(self.fields_view);
+            self.reload();
+        });
+    }
+});
 };
