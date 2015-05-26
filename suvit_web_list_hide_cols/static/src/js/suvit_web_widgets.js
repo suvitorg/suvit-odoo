f044db9af8ada93fe625bc3020364f10a8c94134
Victor Safronovich <vsafronovich@gmail.com>
2015-05-26 11:15:10 +0500

    save to local storage hided cols

--- suvit_web_list_hide_cols/static/src/js/suvit_web_widgets.js
+++ suvit_web_list_hide_cols/static/src/js/suvit_web_widgets.js
@@ -1,22 +1,53 @@
 openerp.suvit_web_widgets = function(instance, local) {
+  var localStorage = {};
+  if (typeof window.localStorage !== "undefined"){
+    localStorage = window.localStorage;
+  }
 
-instance.web.ListView.include({
+  instance.web.ListView.include({
+    load_hide_cols: function(){
+        var data = localStorage[this.fields_view.view_id] || '{}';
+        this.hide_cols = JSON.parse(data);
+    },
+    save_hide_cols: function(data){
+        // data is map of col_name -> col_state
+        localStorage[this.fields_view.view_id] = JSON.stringify(data || this.hide_cols);
+    },
+    setup_columns: function (fields, grouped) {
+        var self = this;
+        this.load_hide_cols();
+
+        _.map(self.fields_view.arch.children, function(field){
+            if (self.hide_cols[field.attrs.name] !== undefined ) {
+                self.add_invisible(field, self.hide_cols[field.attrs.name]);
+            }
+        });
+
+        this._super(fields, grouped);
+    },
+    add_invisible: function(field, is_invisible, save) {
+        var modifiers = JSON.parse(field.attrs.modifiers);
+        modifiers['tree_invisible'] = is_invisible ? '1' : null;
+        field.attrs.modifiers = JSON.stringify(modifiers);
+        this.hide_cols[field.attrs.name] = is_invisible;
+        if (save)
+          this.save_hide_cols();
+    },
     load_list: function(data) {
         var self = this;
+
         this._super(data);
 
         this.$el.find('.oe_view_list_menu li input').click(function(){
             checkbox = $(this);
             _.map(self.fields_view.arch.children, function(field){
                 if (field.attrs.name == checkbox.data('field')) {
-                    var modifiers = JSON.parse(field.attrs.modifiers);
-                    modifiers['tree_invisible'] = !checkbox.prop('checked')? '1': null;
-                    field.attrs.modifiers = JSON.stringify(modifiers);
+                    self.add_invisible(field, !checkbox.prop('checked'), true);
                 }
             });
             self.load_list(self.fields_view);
             self.reload();
         });
     }
-});
+  });
 };
