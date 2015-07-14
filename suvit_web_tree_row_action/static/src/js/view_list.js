76a268d7c643e6efdd665a36b9ebe4c4d62de334
iankin <iankin81@gmail.com>
2015-07-14 22:22:43 +0500

    custom formview action from *2many fields

--- suvit_web_tree_row_action/static/src/js/view_list.js
+++ suvit_web_tree_row_action/static/src/js/view_list.js
@@ -15,4 +15,36 @@ openerp.suvit_web_tree_row_action = function(instance, local) {
 
   });
 
+  var do_action = function(view, id){
+    var model_obj = new instance.web.Model(view.dataset.model);
+    model_obj.call('get_formview_action', [id]).then(function(action){
+      action['target'] = view.ViewManager.field.context.open_formview;
+      view.do_action(action);
+    });
+  };
+
+  instance.web.form.Many2ManyListView.include({
+
+    do_activate_record: function (index, id) {
+        var context = this.ViewManager.field.context;
+        if (!context.open_formview)
+          return this._super(index, id);
+
+        do_action(this, id);
+    }
+
+  });
+
+  instance.web.form.One2ManyListView.include({
+
+    do_activate_record: function(index, id) {
+      var context = this.ViewManager.field.context;
+        if (!context.open_formview)
+          return this._super(index, id);
+
+        do_action(this, id);
+    }
+
+  });
+
 };
