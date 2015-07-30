18f5b2d691e5b96c721b21790e37fe1fd114d409
iankin <iankin81@gmail.com>
2015-07-30 16:25:08 +0500

    focus select page by name in context

--- suvit_web_notebook_focus/static/src/js/suvit_web_widgets.js
+++ suvit_web_notebook_focus/static/src/js/suvit_web_widgets.js
@@ -5,17 +5,28 @@ openerp.suvit_web_notebook_focus = function(instance, local) {
     process_notebook: function($notebook) {
 
       var selected;
+      var pages = $notebook.find('> page');
+      var autofocus_page_name = this.view.ViewManager.action.context.autofocus;
 
-      $notebook.find('> page').each(function(i) {
-        var $page = $(this);
-        var page_attrs = $page.getAttributes();
-        if (page_attrs.autofocus)
-          selected = i;
-      });
+      if (autofocus_page_name){
+        pages.each(function(i) {
+          var $page = $(this);
+          var page_attrs = $page.getAttributes();
+          if (page_attrs.name == autofocus_page_name)
+            selected = i;
+        });
+      } else {
+        pages.each(function(i) {
+          var $page = $(this);
+          var page_attrs = $page.getAttributes();
+          if (page_attrs.autofocus)
+            selected = i;
+        });
+      }
 
       var note = this._super($notebook);
 
-      if (selected !== undefined)
+      if (selected)
         note.tabs('select', selected);
 
       return note;
