69e2c40b73b745cac1bb755c4cbf06cbcc8a0d30
iankin <iankin81@gmail.com>
2015-08-19 19:25:37 +0500

    Many2ManyTreeField move from format_core

--- suvit_web_tree/static/src/js/view_tree.js
+++ suvit_web_tree/static/src/js/view_tree.js
@@ -1,5 +1,6 @@
 openerp.suvit_web_tree = function(instance, local) {
 
+  /********* Custom domain for List & Tree Views in same Action ********/
   instance.web.TreeView.include({
 
     init: function (parent, dataset, view_id, options) {
@@ -13,5 +14,27 @@ openerp.suvit_web_tree = function(instance, local) {
     }
 
   });
+
+  /********* Many2Many Tree Field ********/
+  local.Many2ManyTreeField = instance.web.form.FieldMany2Many.extend({
+
+    initialize_content: function() {
+
+        if (this.field.type != 'many2many')
+            return this._super();
+
+        this.list_view = new instance.web.TreeView(this, this.dataset, false, {
+                    'addable': false,
+                    'deletable': false,
+                    'selectable': false,
+                    'sortable': false,
+                    'reorderable': false,
+                    'import_enabled': false,
+            });
+        this.list_view.appendTo(this.$el);
+    },
+  });
+
+  instance.web.form.widgets.add('many2many_tree', 'instance.format_core.Many2ManyTreeField');
 
 };
