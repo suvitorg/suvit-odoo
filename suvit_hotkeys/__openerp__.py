b8e03c62f2ebb7d0a110d5b8925ebf5080650944
iankin <iankin81@gmail.com>
2015-09-17 19:37:25 +0500

    add suvit_hotkeys module

---
+++ suvit_hotkeys/__openerp__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Сувит. Горячие клавиши',
+    'category': 'Web',
+    'summary': 'Горячие клавиши',
+    'website': 'https://suvit.ru',
+    'version': '1.0',
+    'description': """
+Горячие клавиши
+================================================
+
+        """,
+    'author': 'Suvit LLC',
+    'depends': ['web'],
+    'installable': True,
+    'auto_install': True,
+    'data': [
+        'views/web/assets.xml',
+    ],
+}
---
+++ suvit_hotkeys/static/src/js/suvit_hotkeys.js
@@ -0,0 +1,49 @@
+$.alt_shift = function(key, callback, args) {
+    $(document).keydown(function(e) {
+        if(!args) args=[]; // IE barks when args is null
+        console.log(e.keyCode, key.charCodeAt(0), key);
+        if((e.keyCode == key.charCodeAt(0) || e.keyCode == key) && e.altKey && e.shiftKey) {
+            callback.apply(this, args);
+            return false;
+        }
+    });
+};
+
+//Edit the current object
+$.alt_shift('69', function() {
+  $('.oe_form_button_edit').each(function() {
+    if($(this).parents('div:hidden').length == 0){
+      $(this).trigger('click');
+    }
+  });
+});
+
+
+//Save the current object
+$.alt_shift('83', function() {
+  $('.oe_form_button_save').each(function() {
+    if($(this).parents('div:hidden').length == 0){
+      $(this).trigger('click');
+    }
+  });
+});
+
+//Create new object
+$.alt_shift('67', function() {
+  $('.oe_form_button_create').each(function() {
+    if($(this).parents('div:hidden').length == 0){
+      $(this).trigger('click');
+    }
+  });
+});
+
+//Cancel
+$.alt_shift('68', function() {
+  $('.oe_form_button_cancel').each(function() {
+    if($(this).parents('div:hidden').length == 0){
+      $(this).trigger('click');
+    }
+  });
+});
+
+
