ed884fedd422e0fd80c0069be3a2bb0f9ddf7bb8
Vasiliy <vlobarev@gmail.com>
2015-11-26 16:21:31 +0500

    diagram fix

--- suvit_web_diagram/static/src/js/diagram.js
+++ suvit_web_diagram/static/src/js/diagram.js
@@ -17,39 +17,23 @@ openerp.suvit_web_diagram = function(instance, local) {
         var coords = node_obj.get_pos();
         coords['x'] = coords['x'] - 50;
         coords['y'] = coords['y'] - 50;
-        NodeModel.call('write', [node_obj.id, coords]);
-        // console.log(coords);
-    },
-
-//какой-то костыль, но я не нашел способа сделать лучше.
-    find_label: function(r, el) {
-        self = this;
-        if (r == el) {
-            if (el.next && el.next.type == 'text') {
-                return el.next;
-            }
-            return false;
-        } else if (r.next) {
-            return self.find_label(r.next, el);
-        }
-        return false;
+        // NodeModel.call('write', [node_obj.id, coords]);
+        console.log(coords);
     },
 
     draw_diagram: function(result) {
         var self = this;
         this._super(result);
-        if (self.graph) {
-            _.each(self.graph.get_node_list(), function(n) {
-                var fig = n.get_fig();
-                fig.drag(null, null, function(){
-                    self.save_coords(n);
-                });
-                node_label = self.find_label(self.graph.r.bottom, fig);
-                node_label.drag(null, null, function(){
-                  self.save_coords(n);
-                });
+        _.each(self.graph.get_node_list(), function(n) {
+            var fig = n.get_fig();
+            fig.drag(null, null, function(){
+                self.save_coords(n);
             });
-        }
+            node_label = self.graph.r.getById(fig.id).next;
+            node_label.drag(null, null, function(){
+              self.save_coords(n);
+            });
+        });
     }
   });
 };
