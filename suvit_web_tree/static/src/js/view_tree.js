5cae6317fd16d0ecdd66c3a98deb16a5f0976f09
Vasiliy <vlobarev@gmail.com>
2015-12-08 15:52:25 +0500

    обновление дерева в карточке

--- suvit_web_tree/static/src/js/view_tree.js
+++ suvit_web_tree/static/src/js/view_tree.js
@@ -8,8 +8,21 @@ openerp.suvit_web_tree = function(instance, local) {
           var domain = new instance.web.CompoundDomain(dataset.domain, parent.action.context.tree_domain);
           dataset = new instance.web.DataSetSearch(this, parent.action.res_model, dataset.context, domain);
         }
-
         this._super(parent, dataset, view_id, options);
+
+        self = this;
+        this.ViewManager.on("switch_mode", self, function(mode) {
+          if (mode == 'tree') this.switch_mode();
+        });
+    },
+    switch_mode: function () {
+      this.$el.find(".oe-treeview-table > tbody").empty();
+      _(this.fields_view.arch.children).each(function (field) {
+          if (field.attrs.modifiers && typeof(field.attrs.modifiers) == 'object') {
+              field.attrs.modifiers = JSON.stringify(field.attrs.modifiers);
+          }
+      });
+      this.view_loading(this.fields_view);
     },
     hook_row_click: function () {
       this.$el.undelegate('.treeview-tr', 'click');
@@ -19,12 +32,9 @@ openerp.suvit_web_tree = function(instance, local) {
 
   /********* Many2Many Tree Field ********/
   local.Many2ManyTreeField = instance.web.form.FieldMany2Many.extend({
-
     initialize_content: function() {
-
         if (!(this.field.type == 'many2many' || this.field.type == 'one2many'))
             return this._super();
-
         this.list_view = new instance.web.TreeView(this, this.dataset, false, {
                     'addable': false,
                     'deletable': false,
@@ -33,35 +43,14 @@ openerp.suvit_web_tree = function(instance, local) {
                     'reorderable': false,
                     'import_enabled': false,
             });
+        this.$el.empty();
         this.list_view.appendTo(this.$el);
     },
-  });
-
-  instance.web.form.widgets.add('many2many_tree', 'instance.suvit_web_tree.Many2ManyTreeField');
-
-  instance.web.ViewManager.include({
-    init: function(parent, dataset, view_id, options) {
-      self = this;
-      this._super(parent, dataset, view_id, options);
-      this.on('switch_mode', self, function(mode) {
-        if (mode == 'tree') {
-          if (self.views) {
-            tmp = self;
-          } else {
-            tmp = self.ViewManager;
-          }
-          tmp.views[mode].controller.$el.find(".oe-treeview-table > tbody").empty();
-          var fields_view = tmp.views[mode].controller.fields_view;
-          _(fields_view.arch.children).each(function (field) {
-              if (field.attrs.modifiers && typeof(field.attrs.modifiers) == 'object') {
-                  field.attrs.modifiers = JSON.stringify(field.attrs.modifiers);
-              }
-          });
-          tmp.views[mode].controller.view_loading(fields_view);
-
-        }
-      });
+    render_value: function() {
+      this.initialize_content();
+      return this._super();
     }
   });
 
+  instance.web.form.widgets.add('many2many_tree', 'instance.suvit_web_tree.Many2ManyTreeField');
 };
