6451440e673885a4b702bd764d9f60ca778615a2
Victor Safronovich <vsafronovich@gmail.com>
2016-03-11 10:18:00 +0500

    added ability do extract last id from full_id with parents

--- suvit_web_tree/static/src/js/view_tree.js
+++ suvit_web_tree/static/src/js/view_tree.js
@@ -38,16 +38,26 @@ openerp.suvit_web_tree = function(instance, local) {
       this.$el.undelegate('.treeview-tr', 'click');
       this._super();
     },
+  });
+
+  /* Ability
+     1. to send all node`s parents to server, to can filter children depends on parent
+     2. server may response with id with all parents, so extract only last id */
+  instance.web.TreeView.include({
     getparents: function(id) {
+      /* by default row id is integer server id,
+         but we can modify id too support same tree node in different branches,
+         so use last integer number in id(example 2-22-2222) */
       var parent_ids = [],
           $parent_row = this.$el.find('[data-id='+ id +']');
       while ($parent_row.length) {
-        parent_ids.unshift($parent_row.data('id'));
+        parent_ids.unshift($parent_row.data('id').toString().split('-').pop());
         $parent_row = this.$el.find('[data-id='+ $parent_row.data('row-parent-id') +']');
       }
       return parent_ids;
     },
     getdata: function (id, children_ids) {
+      /********* Send perents in context ********/
       var self = this;
       if (!self.real_context)
         self.real_context = self.dataset._model._context;
