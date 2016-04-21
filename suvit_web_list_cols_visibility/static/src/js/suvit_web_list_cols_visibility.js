001c045da428843fc373e34d7796be06f740f4c5
Vasiliy <vlobarev@gmail.com>
2016-04-21 08:08:17 +0500

    разделение модулей

---
+++ suvit_web_list_cols_visibility/static/src/js/suvit_web_list_cols_visibility.js
@@ -0,0 +1,43 @@
+openerp.suvit_web_list_cols_visibility = function(instance, local) {
+  var localStorage = window['localStorage'] || {},
+      QWeb = instance.web.qweb;
+
+  instance.web.ListView.include({
+    setup_columns: function (fields, grouped) {
+        var self = this;
+        this.load_hide_cols();
+        self.fields_addition_class = {};
+        _.map(self.fields_view.arch.children, function(field){
+            if (self.hide_cols[field.attrs.name] !== undefined ) {
+                self.add_invisible(field, self.hide_cols[field.attrs.name]);
+            }
+            if (field.attrs.class) {
+                self.fields_addition_class[field.attrs.name] = field.attrs.class;
+            }
+        });
+        this._super(fields, grouped);
+    }
+  });
+  instance.web.ListView.List.include({
+    render_record: function (record) {
+        self = this;
+        classes = self.view.fields_addition_class;
+        r = this._super(record);
+        d = $(r);
+        for (field in classes) {
+            d.find('.oe_list_field_cell[data-field="'+field+'"]').addClass(classes[field]);
+        }
+        return d;
+    },
+    render: function () {
+        self = this;
+        classes = self.view.fields_addition_class;
+        self._super();
+        for (field in classes) {
+            self.$current.find('.oe_list_field_cell[data-field="'+field+'"]').addClass(classes[field]);
+            var ind = self.$current.find('.oe_list_field_cell').index(self.$current.find('.oe_list_field_cell[data-field="'+field+'"]'));
+            self.view.$el.find('th[data-id="'+field+'"], .oe_list_footer:eq('+ind+')').addClass(classes[field]);
+        }
+    },
+  });
+};
