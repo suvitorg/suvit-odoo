9ec0fdaa8a8bdeb61d489bd361cb97115089cb85
Victor Safronovich <vsafronovich@gmail.com>
2016-11-12 14:11:25 +0500

    added module that update modules list

---
+++ suvit_base_app_list_update/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'СУВИТ. Обновление списка модулей',
+    'category': 'Hidden',
+    'version': '1.0',
+    'description':
+        """
+        """,
+    'depends': ['base'],
+    'data': [
+        'views/update_modules.xml',
+    ],
+    'qweb': [
+    ],
+    #'auto_install': True,
+    'bootstrap': True,  # load translations for login screen
+}
