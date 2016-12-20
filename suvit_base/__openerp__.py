99864347467a477df80737107305ead59323dc17
Victor Safronovich <vsafronovich@gmail.com>
2016-12-20 13:15:49 +0500

    added patch for mail_thread to support unicode field names

--- suvit_base/__openerp__.py
+++ suvit_base/__openerp__.py
@@ -25,6 +25,9 @@ TODO
                 # 3. support_debranding
                 # 4. hide_db_manager_link oca/server_tools
                 'suvit_sentry',
+
+                # for patch
+                'mail',
                 ],
     'data': [
     ],
--- suvit_system/tests/test_node.py
+++ suvit_system/tests/test_node.py
@@ -6,16 +6,18 @@ class TestNode(TransactionCase):
     def test_node_crud(self):
         Node = self.env['suvit.system.node']
 
-        root = Node.create({'name': u'Root1'})
+        root = Node.create({'name': 'Root1'})
 
-        ch1 = Node.create({'name': u'Ch1', 'parent_id': root.id})
-        ch2 = Node.create({'name': u'Ch3', 'parent_id': root.id})
-        ch3 = Node.create({'name': u'Ch2', 'parent_id': root.id})
+        ch1 = Node.create({'name': 'Ch1', 'parent_id': root.id})
+        ch2 = Node.create({'name': 'Ch3', 'parent_id': root.id})
+        ch3 = Node.create({'name': 'Ch2', 'parent_id': root.id})
 
-        ch2.write({'name': u'Ch3-last', 'sequence': 1000})
+        ch2.write({'name': 'Ch3-last', 'sequence': 1000})
 
         self.assertEqual(len(root.child_ids), 3)
-        self.assertEqual(root.child_ids.mapped('name'), [u'Ch1', u'Ch2', u'Ch3-last'])
+        # test change name and order of children
+        self.assertEqual(root.child_ids.mapped('name'),
+                         ['Ch1', 'Ch2', 'Ch3-last'])
 
         ch3.unlink()
 
@@ -23,6 +25,6 @@ class TestNode(TransactionCase):
         ch2.action_duplicate()
 
         # add children for ch2
-        ch2_1 = Node.create({'name': u'Ch2-1', 'parent_id': ch2.id})
-        ch2_2 = Node.create({'name': u'Ch2-2', 'parent_id': ch2.id})
+        ch2_1 = Node.create({'name': 'Ch2-1', 'parent_id': ch2.id})
+        ch2_2 = Node.create({'name': 'Ch2-2', 'parent_id': ch2.id})
 
