0dfeb68f246d53be87305f05b637de02e5955060
Victor Safronovich <vsafronovich@gmail.com>
2016-12-22 17:18:32 +0500

    added test to copy/duplicate nodes

--- suvit_system/tests/test_node.py
+++ suvit_system/tests/test_node.py
@@ -28,3 +28,29 @@ class TestNode(TransactionCase):
         ch2_1 = Node.create({'name': 'Ch2-1', 'parent_id': ch2.id})
         ch2_2 = Node.create({'name': 'Ch2-2', 'parent_id': ch2.id})
 
+    def test_copy_duplicate():
+        Node = self.env['suvit.system.node']
+
+        root = Node.create({'name': 'Root1'})
+
+        root.action_duplicate()
+
+        duplicate = Node.search([('name', 'like', Node._duplicate_prefix)])
+        self.assertEqual(len(duplicate), 1)
+        self.assertEqual(duplicate.name, 'D_Root1')
+        self.assertEqual(duplicate.shortcut_id, root)
+        self.assertEqual(duplicate.self_id, root)
+        self.assertEqual(root.self_id, root)
+
+        # test copy of duplicate is a copy of original
+        duplicate.action_copy()
+
+        # root has one duplicate
+        self.assertEqual(root.duplicate_ids.ids, [duplicate.id])
+        self.assertEqual(Node.search([], count=True), 3)
+
+        copy = Node.search([('name', 'like', Node._copy_suffix)])
+        self.assertEqual(len(copy), 1)
+        self.assertEqual(duplicate.name, u'D_Root1 Копия')
+        self.assertEqual(copy.shortcut_id, False)
+        self.assertEqual(copy.self_id, copy)
