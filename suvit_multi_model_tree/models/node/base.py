a1e791ed7cddbf5488bf2d9e8fa3621575e999d0
Victor Safronovich <vsafronovich@gmail.com>
2016-12-23 10:12:06 +0500

    split node.py into pieces

--- suvit_multi_model_tree/models/node/base.py
+++ suvit_multi_model_tree/models/node/base.py
@@ -219,7 +219,6 @@ class TreeNode(models.AbstractModel):
 
     @api.multi
     def action_change_parent(self):
-
         new_parent_id = self.env.context.get('new_parent_id')
         if new_parent_id:
             new_parent = self.browse(new_parent_id).self_id
@@ -281,50 +280,6 @@ class TreeNode(models.AbstractModel):
         else:
             # XXX recalc whole tree
             self._parent_store_compute()
-
-    # API for full ids
-    @api.multi
-    def evaluate_ids(self):
-        if not any(isinstance(id, basestring) for id in self.ids):
-            return
-
-        new_ids = []
-        for id in self.ids:
-            new_ids.append(int(str(id).split('-')[-1]))
-
-        # XXX This is needed to clear cache string ids
-        self.invalidate_cache()
-        self._ids = new_ids
-
-    @api.multi
-    def read(self, fields=None, *args, **kwargs):
-        if not self._use_full_ids:
-            return super(TreeNode, self).read(fields, *args, **kwargs)
-
-        self.evaluate_ids()
-
-        res = super(TreeNode, self).read(fields, *args, **kwargs)
-
-        parents = self.env.context.get('tree_parent_ids')
-        if parents:
-            parent_prefix = '-'.join(str(x) for x in parents)
-        else:
-            parent_prefix = None
-
-        for row in res:
-            if parent_prefix:
-                row['real_id'] = row['id']
-                row['id'] = '%s-%s' % (parent_prefix, row['id'])
-
-            if 'tree_child_ids' in row:
-                new_children = []
-                for child in row['tree_child_ids']:
-                    new_children.append('%s-%s' % (row['id'],
-                                                   child
-                                                   if isinstance(child, int)
-                                                   else child.id))
-                row['tree_child_ids'] = new_children
-        return res
 
     @api.multi
     def get_formview_action(self):
