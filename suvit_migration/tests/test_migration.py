c5e3f8f0de84b147de605769e8f7ec0792612be8
Victor Safronovich <vsafronovich@gmail.com>
2016-12-26 21:19:06 +0500

    added test for migration

---
+++ suvit_migration/tests/test_migration.py
@@ -0,0 +1,14 @@
+﻿# -*- coding: utf-8 -*-
+from openerp.tests.common import TransactionCase
+
+
+class TestNode(TransactionCase):
+    def test_migration(self):
+        Migration = self.env['suvit.migration']
+
+        migration = Migration.create({'name': 'Тест',
+                                      'method': 'read'})
+
+        migration.run()
+
+        self.assertTrue(migration.implemented)
