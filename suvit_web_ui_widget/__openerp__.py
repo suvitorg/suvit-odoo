c794638a5a73e32d0b3411e34147dbcbf26514e5
Victor Safronovich <vsafronovich@gmail.com>
2017-04-06 16:14:16 +0500

    added first version of widget dict

---
+++ suvit_web_ui_widget/__openerp__.py
@@ -0,0 +1,27 @@
+{
+    'name': 'Сувит. Виджеты',
+    'category': 'UI',
+    'summary': 'Модель Виджета для Odoo',
+    'website': 'https://suvit.ru',
+    'author': 'Suvit LLC',
+    'version': '1.0',
+    'description': """
+Модель Виджета для Odoo
+===========================
+Дерево виджетов
+
+        """,
+    'depends': [
+        'web_widget_text_markdown',  # oca/web
+        'suvit_multi_model_tree', # for widget tree
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+
+        'views/widget_type.xml',
+        'views/widget_type_tree.xml',
+        'views/menu.xml',
+    ],
+    'installable': True,
+    # 'auto_install': True,
+}
