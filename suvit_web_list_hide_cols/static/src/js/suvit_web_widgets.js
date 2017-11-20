ac4f2ad08ac025e4b18f158684eb68aea9f03755
iankin <iankin81@gmail.com>
2017-11-20 18:12:36 +0500

    btn_hide_cols click in form read mode. undone. need to return original event propagation

--- suvit_web_list_hide_cols/static/src/js/suvit_web_widgets.js
+++ suvit_web_list_hide_cols/static/src/js/suvit_web_widgets.js
@@ -54,6 +54,10 @@ openerp.suvit_web_list_hide_cols = function(instance, local) {
             self.load_list(self.fields_view);
             self.reload();
         });
+
+        this.$pager.find("button.btn_hide_cols").on('click', function (event) {
+            console.log(event);
+        });
     }
   });
 };
