a39e4dff0b3375f6cd95400284a7038db665d2a3
Alexander Skiba <rhcp-sk@mail.ru>
2017-11-22 16:08:09 +0500

    Hide cols menu done in listView and formView

--- suvit_web_list_hide_cols/static/src/js/suvit_web_widgets.js
+++ suvit_web_list_hide_cols/static/src/js/suvit_web_widgets.js
@@ -3,13 +3,19 @@ openerp.suvit_web_list_hide_cols = function(instance, local) {
       QWeb = instance.web.qweb;
 
   instance.web.ListView.include({
-    load_hide_cols: function(){
-        var data = localStorage[this.fields_view.view_id] || '{}';
+    hide_cols_id: function() {
+      var id = this.fields_view.view_id
+      if(this.ViewManager.field_manager){
+        id = this.ViewManager.field_manager.fields_view.view_id+'_'+this.ViewManager.name;
+      }
+      return id;
+    },
+    load_hide_cols: function() {
+        var data = localStorage[this.hide_cols_id()] || '{}';
         this.hide_cols = JSON.parse(data);
     },
-    save_hide_cols: function(data){
-        // data is map of col_name -> col_state
-        localStorage[this.fields_view.view_id] = JSON.stringify(data || this.hide_cols);
+    save_hide_cols: function(data) {
+        localStorage[this.hide_cols_id()] = JSON.stringify(data || this.hide_cols);
     },
     add_invisible: function(field, is_invisible, save) {
         var modifiers = JSON.parse(field.attrs.modifiers);
@@ -22,24 +28,24 @@ openerp.suvit_web_list_hide_cols = function(instance, local) {
     },
     load_list: function(data) {
         var self = this;
-
         this._super(data);
-
-        var $menu = this.$pager.find('.oe_view_hide_cols_menu');
-        if (!$menu.size() && !this.o2m) {
-          this.$pager.prepend(QWeb.render("ListView.hide_cols", this));
+        var $sidebar = this.ViewManager.field_manager ? $('.oe_list_sidebar') : $('.oe_sidebar');
+        
+        var $menu = $sidebar.find('.oe_view_hide_cols_menu');
+        if (!$menu.size()) {
+          $sidebar.append(QWeb.render("ListView.hide_cols", this));
         }
-
+        
         if ( $('.oe_view_hide_cols_menu li input:checked').length <= 1 ) {
             $('.oe_view_hide_cols_menu li input:checked').prop('disabled', true);
         } else {
             $('.oe_view_hide_cols_menu li input').prop('disabled', false);
         }
         $('.oe_view_hide_cols_menu li input[data-swichable = "0"]').prop('disabled', true);
-        this.$pager.find('.oe_view_hide_cols_menu li input').off('click').on('click',function(event){
+        this.$el.find('.oe_view_hide_cols_menu li input').off('click').on('click',function(event){
             $checkbox = $(this);
             if ($checkbox.data('swichable') === 0) {
-                event.preventDefault()
+                event.preventDefault();
                 return false;
             }
             _.map(self.fields_view.arch.children, function(field){
@@ -47,17 +53,24 @@ openerp.suvit_web_list_hide_cols = function(instance, local) {
                     self.add_invisible(field, !$checkbox.prop('checked'), true);
                 }
             });
+            
             if ($checkbox.data('field') == '_row_no') {
                 self.hide_cols['_row_no'] = !$checkbox.prop('checked');
                 self.save_hide_cols();
             };
+            self.$pager = null;
             self.load_list(self.fields_view);
             self.reload();
         });
+        
+        var hide_cols_menu = $('.oe_view_hide_cols_menu');
+        this.ViewManager.field_manager ? hide_cols_menu.addClass('oe_left') : hide_cols_menu.css({"text-align": "center", "vertical-align": "top"});
 
-        this.$pager.find("button.btn_hide_cols").on('click', function (event) {
-            console.log(event);
-        });
+        if(this.ViewManager.field_manager){
+          this.$el.find('button.btn_hide_cols').on('click', function (event) {
+            $('.oe_view_hide_cols_menu').toggleClass('open');
+          });
+        }        
     }
   });
 };
