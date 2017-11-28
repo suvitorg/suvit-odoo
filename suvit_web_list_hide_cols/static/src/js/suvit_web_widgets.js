616bfe4d8697d0cb3a6f2f0789ba03f2acdbe207
Alexander Skiba <rhcp-sk@mail.ru>
2017-11-28 13:15:18 +0500

    Works both in ListView and FormView

--- suvit_web_list_hide_cols/static/src/js/suvit_web_widgets.js
+++ suvit_web_list_hide_cols/static/src/js/suvit_web_widgets.js
@@ -36,19 +36,20 @@ openerp.suvit_web_list_hide_cols = function(instance, local) {
         var self = this;
         this._super(data);
         var $sidebar = this.is_inside_form() ? $('.oe_list_sidebar') : $('.oe_sidebar');
-        
+
         var $menu = $sidebar.find('.oe_view_hide_cols_menu');
         if (!$menu.size()) {
           $sidebar.append(QWeb.render("ListView.hide_cols", this));
+          $menu = $sidebar.find('.oe_view_hide_cols_menu');
         }
-        
+
         if ( $('.oe_view_hide_cols_menu li input:checked').length <= 1 ) {
             $('.oe_view_hide_cols_menu li input:checked').prop('disabled', true);
         } else {
             $('.oe_view_hide_cols_menu li input').prop('disabled', false);
         }
         $('.oe_view_hide_cols_menu li input[data-swichable = "0"]').prop('disabled', true);
-        this.$el.find('.oe_view_hide_cols_menu li input').off('click').on('click',function(event){
+        $('.oe_view_hide_cols_menu li input').off('click').on('click',function(event){
             $checkbox = $(this);
             if ($checkbox.data('swichable') === 0) {
                 event.preventDefault();
@@ -59,26 +60,28 @@ openerp.suvit_web_list_hide_cols = function(instance, local) {
                     self.add_invisible(field, !$checkbox.prop('checked'), true);
                 }
             });
-            
+
             if ($checkbox.data('field') == '_row_no') {
                 self.hide_cols['_row_no'] = !$checkbox.prop('checked');
                 self.save_hide_cols();
             };
-            self.$pager = null;
+            if(self.is_inside_form()){
+              self.$pager = null;
+            }
             self.load_list(self.fields_view);
             self.reload();
         });
-        
-        $menu.selector === '.oe_list_sidebar .oe_view_hide_cols_menu' ? $(''+$menu.selector+'').addClass('oe_left') : $(''+$menu.selector+'').css({"text-align": "center", "vertical-align": "top"});
+
+        if(this.is_inside_form())
+          $menu.addClass('oe_left')
+        else
+          $menu.css({"text-align": "center", "vertical-align": "top", "margin-top": "1px"});
 
         if(this.is_inside_form()){
           this.$el.find('button.btn_hide_cols').on('click', function (event) {
-            $(''+$menu.selector+'').toggleClass('open');
+            $menu.toggleClass('open');
           });
-        }        
-        //this.$pager.find("button.btn_hide_cols").on('click', function (event) {
-        //    console.log(event);
-        //});
+        }
     }
   });
 };
