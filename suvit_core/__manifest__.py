4496790e7a83d7c066b655c1ec9b97e3bec14f77
Michail_Z <apxahrejl@gmail.com>
2018-01-15 08:39:49 +0500

    Added suvit_core module

---
+++ suvit_core/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Taxn. Ядро',
+    'version': '0.0.1',
+    'category': 'TAXN',
+    'complexity': 'easy',
+    'description': """
+TODO
+    """,
+    'author': 'Suvit LLC',
+    'depends': [
+        'mail',
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'js': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'qweb': [
+    ],
+}
