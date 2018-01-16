7441ce727e45f88893e17074d06051bc4ece0c64
vitzgrk <vzagoruyko@suvit.yaconnect.ru>
2018-01-16 14:31:33 +0500

    add new module 'suvit_support'

---
+++ suvit_support/__openerp__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Поддержка',
+    'category': 'Website',
+    'summary': 'Виджеты для Odoo',
+    'website': 'https://suvit.ru',
+    'version': '1.0',
+    'description': "Редирект на https://suvit.ru",
+    'author': 'Suvit LLC',
+    'depends': ['web'],
+    'installable': True,
+    'auto_install': True,
+
+    'data': [
+        'suvit_support.xml',
+    ],
+}
---
+++ suvit_support/static/src/js/suvit_support.js
@@ -0,0 +1,10 @@
+odoo.define('suvit_support.Support', function(require){
+  var UserMenu = require('web.UserMenu')
+  var Custom_Menu = UserMenu.include({
+    template: 'UserMenu',
+    on_menu_support: function(){      
+      window.open('https://suvit.ru', '_blank')
+    }
+  })
+return Custom_Menu
+})
