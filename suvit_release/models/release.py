cb0fbc3d4139ca77df36db8171bc7b37eeb50c1a
vitzgrk <vit.zgrk@gmail.com>
2018-01-17 10:10:46 +0500

    Update release.py

--- suvit_release/models/release.py
+++ suvit_release/models/release.py
@@ -6,15 +6,15 @@ logger = logging.getLogger(__name__)
 
 class Release(models.Model):
     _name = 'suvit.release'
-	_description = u"Релиз"
-	_inherit = 'mail.thread'
-
-	name = fields.Integer(string=u"Номер релиза")
-	description = fields.Text(string=u"Описание")
-
-	@api.model
-	def create(self, values):
-		rec = super(Release, self).create(values)
-		group = self.env['mail.group'].search([('name', '=', u'Обновления')])
-		group.message_post(body=values['description'])
-		return rec
+    _description = u"Релиз
+    _inherit = 'mail.thread'
+    
+    name = fields.Integer(string=u"Номер релиза")
+    description = fields.Text(string=u"Описание")
+    
+    @api.model
+    def create(self, values):
+        rec = super(Release, self).create(values)
+        group = self.env['mail.group'].search([('name', '=', u'Обновления')])
+        group.message_post(body=values['description'])
+        return rec
