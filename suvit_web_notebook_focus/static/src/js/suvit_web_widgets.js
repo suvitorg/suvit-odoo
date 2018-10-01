278a57af439dffdb8ae64a572c04d99674ed4d2c
iankin <iankin81@gmail.com>
2018-10-01 19:08:03 +0500

    odoo 10 notebook page autofocus

--- suvit_web_notebook_focus/static/src/js/suvit_web_widgets.js
+++ suvit_web_notebook_focus/static/src/js/suvit_web_widgets.js
@@ -1,31 +1,31 @@
-openerp.suvit_web_notebook_focus = function(instance, local) {
-  //TODO odoo10.0
-  return;
+odoo.define('web.autofocus', function (require) {
+"use strict";
 
-  instance.web.form.FormRenderingEngine.include({
+  var FormRenderingEngine = require('web.FormRenderingEngine');
+
+  FormRenderingEngine.include({
 
     process_notebook: function($notebook) {
 
-      var selected;
-      var pages = $notebook.find('> page');
       var action = this.view.ViewManager.action;
       var af_name = action && action.context.autofocus;
+      if (!af_name)
+        this._super($notebook);
 
+      var pages = $notebook.find('> page');
       pages.each(function(i) {
           var $page = $(this);
           var page_attrs = $page.getAttributes();
-          if (page_attrs.autofocus || (af_name && page_attrs.name == af_name))
-            selected = i;
+          if (page_attrs.autofocus && page_attrs.name != af_name){
+            $page.removeAttr('autofocus');
+          } else if (page_attrs.name == af_name && !page_attrs.autofocus){
+            $page.attr('autofocus', true);
+          }
       });
 
-      var note = this._super($notebook);
-
-      if (selected)
-        note.tabs('select', selected);
-
-      return note;
+      return this._super($notebook);
 
     }
   });
 
-};
+});
