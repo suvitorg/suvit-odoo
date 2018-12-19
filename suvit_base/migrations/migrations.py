8ae4fcd6837cbbb060b05dcfd72950a7de619382
Michail Zotin <apxahrejl@gmail.com>
2018-12-19 11:38:16 +0500

    Migration corrected

--- suvit_base/migrations/migrations.py
+++ suvit_base/migrations/migrations.py
@@ -24,12 +24,22 @@ class CurrencyMigration(models.Model):
         res_models_to_remove = list(set(Followers.search([('res_model', 'not in', self.env.keys())]).mapped('res_model')))
         if res_models_to_remove:
             query_delete_records(cr, res_models_to_remove, "res_model", "mail_followers")
-        Followers.search([('res_id', '=', False)]).unlink()
+        for rec in Followers.search([]):
+            if not self.env[rec.res_model].browse(rec.res_id):
+                _logger.debug("Delete mail followers record with id: %d related to res_model: %s, res_id: %d" % (
+                    rec.id, rec.res_model, rec.res_id))
+                rec.unlink()
 
         Messages = self.env['mail.message']
         cr = Messages._cr
         res_models_to_remove = list(set(Messages.search([('model', 'not in', self.env.keys())]).mapped('model')))
-        res_models_to_remove.remove(False)  # Выяснилось, что в сообщениях есть записи с пустым model.
+        if False in res_models_to_remove:
+            res_models_to_remove[res_models_to_remove.index(False)] = u''
         if res_models_to_remove:
             query_delete_records(cr, res_models_to_remove, "model", "mail_message")
-        Messages.search([('res_id', '=', False)]).unlink()
+        Messages.search([('model', '=', False)]).unlink()
+        for rec in Messages.search([]):
+            if not self.env[rec.model].browse(rec.res_id):
+                _logger.debug("Delete mail message record with id: %d related to model: %s, res_id: %d" % (
+                    rec.id, rec.model, rec.res_id))
+                rec.unlink()
