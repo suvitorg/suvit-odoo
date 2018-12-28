c3bf889d4252a9bbd7a0cca7226530bcc36b368a
iankin <ankinilya@ya.ru>
2018-12-28 18:12:50 +0500

    fix description

--- module_fast_update/__manifest__.py
+++ module_fast_update/__manifest__.py
@@ -5,6 +5,7 @@
     'version': '0.0.1',
     'description': """
         Update modudle without depended modules through command line. Example: odoo-bin -c odoo.conf -d 10 -u module_fast_update:fast
+        Need to be added to config server_wide_modules
     """,
     'author': 'Suvit LLC',
     'depends': [
