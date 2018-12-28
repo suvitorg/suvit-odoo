5ae94e05e45a0ccac21e0541172cb71afda04480
iankin <ankinilya@ya.ru>
2018-12-28 18:03:21 +0500

    add module module_fast_update

---
+++ module_fast_update/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Suvit. Module Fast Update',
+    'category': 'SUVIT',
+    'website': 'https://suvit.ru',
+    'version': '0.0.1',
+    'description': """
+        Update modudle without depended modules through command line. Example: odoo-bin -c odoo.conf -d 10 -u module_fast_update:fast
+    """,
+    'author': 'Suvit LLC',
+    'depends': [
+    ],
+    'data': [
+    ],
+    'qweb': [
+    ],
+    'installable': True,
+    'auto_install': False,
+}
