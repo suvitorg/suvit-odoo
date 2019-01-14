c711a985dc0ad80890e72008a7cdfdcc7101e3e7
iankin <ankinilya@ya.ru>
2019-01-14 14:32:43 +0500

    restore suvit_mass_editing

--- suvit_mass_editing/models/__init__.py
+++ suvit_mass_editing/models/__init__.py
@@ -1,11 +1,1 @@
 from . import mass_object
-
-# Patch
-from odoo.addons.mass_editing.models import ir_model_fields
-
-
-def fixed_list(ids_str):
-    return any(id_str.isdigit() for id_str in ids_str.split(','))
-
-# XXX fixed ugly error in mass_editing field.search method
-ir_model_fields.list = fixed_list
