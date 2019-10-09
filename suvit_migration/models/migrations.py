5800175d18a0b8da04eb2a1eed4e5c28bfd8dacc
godjango <mironkez@yandex.ru>
2019-10-09 09:14:47 +0500

    correct run migration logic

--- suvit_migration/models/migrations.py
+++ suvit_migration/models/migrations.py
@@ -70,16 +70,10 @@ class Migration(models.Model):
     @api.multi
     def run(self):
         now = fields.Date.today()
-        
-        print('='*50)
-        print(os.environ.get('RUN_MIGRATION', False))
-        print(tools.config.options['test_enable'])
-        print(os.environ.get('GITLAB_CI', False))
-        print('='*50)
 
         if not os.environ.get('RUN_MIGRATION', False) \
-            and tools.config.options['test_enable'] \
-            or os.environ.get('GITLAB_CI', False):
+            and (tools.config.options['test_enable'] \
+                 or os.environ.get('GITLAB_CI', False)):
             self.write({'state': 'done',
                         'date_done': now
                         # TODO add flag autodone
